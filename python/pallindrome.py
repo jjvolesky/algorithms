cf2ff15a8adb836b86a834be39d3f8b681a835b9
jjvolesky <30hostilechicken03@gmail.com>
2023-09-01 10:49:48 -0600

    write pallindrome.py

---
+++ python/pallindrome.py
@@ -0,0 +1,11 @@
+import sys
+
+def pallindrome(str):
+    return str.lower() == str[::-1].lower()
+
+def main():
+    args = sys.argv[1:]
+    return pallindrome(args[0])
+
+if __name__ == "__main__":
+    print(main())
